74094d3d60cd8c9e1824900289a20a0cdba17906
Deepti <dsunderp@andrew.cmu.edu>
2013-09-12 21:31:10 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 App1::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
+ root 'static_pages#home'
+  match '/signup',   to: 'users#new',   via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :posts
 
   resources :users
